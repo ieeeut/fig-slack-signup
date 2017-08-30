e123c3e291aec7d382a533294653ce6fbc37d576
patrickedelen <patrickedelen@gmail.com>
2017-08-30 15:31:54 -0500

    added new frontend theme to signup site

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const request = require('request');
+const bodyParser = require('body-parser');
+
+const figNames = [
+    'bits-and-bytes',
+    'the-comm-nets',
+    'digital-impact',
+    'electromagnets',
+    'circuit-breakers',
+    'resistance',
+    'transformers',
+    'power-up',
+    'integrated-circuits',
+    'designers',
+    'short-circuits',
+    'achievers'
+];
 
 const slacktoken = 'secret';
 
@@ -9,15 +25,33 @@ const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
 
 // routes for twilio
 app.post('/adduser', function(req, res, next) {
-    let body = req.body.body;
+    console.log(req.body.email);
+    if (req.body.fig) {
+        console.log('fig name: ' + req.body.fig);
+        console.log('channelId: ' + req.body.chan);
+    } else {
+        console.log('User joined from main page');
+    }
     //get all info on the new user and add them to the channel
+    res.redirect('/submitted');
 });
 
 app.get('/', function(req, res) {
     res.render('index', {}, function(err, html) {
+
+        res.send(html);
+    });
+});
+
+app.get('/submitted', function(req, res) {
+    res.render('submitted', {}, function(err, html) {
         if (err) {
             console.log(err);
         }
@@ -26,32 +60,64 @@ app.get('/', function(req, res) {
     });
 });
 
+app.get('/notfound', function(req, res) {
+    res.render('notfound', {}, function(err, html) {
+        if (err) {
+            console.log(err);
+        }
+
+        res.send(html);
+    });
+});
+
+app.get('/favicon.ico', function(req, res) {
+    res.status(404).send({
+        msg: 'Not Found'
+    });
+});
+
 app.get('/:figname', function(req, exRes) {
     let figName = req.params.figname;
-    let channelListUrl = 'https://slack.com/api/channels.list?token=' + slacktoken; 
-    request(channelListUrl, function(err, res, body) {
-        let bodyJson = JSON.parse(body);
-        let channelCreated = false;
-        let channelId = null;
 
-        if (bodyJson.ok) {
-            bodyJson.channels.forEach(function(elem) {
-                if (elem.name == figName) {
-                    channelCreated = true;
-                    channelId = elem.id;
-                    console.log('channel already exists');
-                }
-            }, this);
+    if (figNames.indexOf(figName) > -1) {
+        console.log('found fig');
 
-            if (!channelCreated) {
-                let channelCreationUrl = 'https://slack.com/api/channels.join?token=' + slacktoken + '&name=' + figName;
-                request(channelCreationUrl, function(err, res, bodyChanCreated) {
-                    let bodyChanJson = JSON.parse(bodyChanCreated);
-                    console.log(bodyChanCreated);
-                    //console.log(bodyChanJson.channel.id);
-                    //channelId = bodyChanJson.channel.id;
-
-                    console.log('channel created, rendering');
+        console.log('creating ' + figName);
+        let channelListUrl = 'https://slack.com/api/channels.list?token=' + slacktoken; 
+        request(channelListUrl, function(err, res, body) {
+            let bodyJson = JSON.parse(body);
+            let channelCreated = false;
+            let channelId = null;
+    
+            if (bodyJson.ok) {
+                bodyJson.channels.forEach(function(elem) {
+                    if (elem.name == figName) {
+                        channelCreated = true;
+                        channelId = elem.id;
+                        console.log('channel already exists');
+                    }
+                }, this);
+    
+                if (!channelCreated) {
+                    let channelCreationUrl = 'https://slack.com/api/channels.join?token=' + slacktoken + '&name=' + figName;
+                    request(channelCreationUrl, function(err, res, bodyChanCreated) {
+                        let bodyChanJson = JSON.parse(bodyChanCreated);
+                        let channelId = 'test';
+                        console.log(bodyChanCreated);
+                        //console.log(bodyChanJson.channel.id);
+                        //channelId = bodyChanJson.channel.id;
+    
+                        console.log('channel created, rendering');
+                        exRes.render('figsignup', {figname: figName, chan: channelId}, function(err, html) {
+                            if (err) {
+                                console.log(err);
+                            }
+                    
+                            exRes.send(html);
+                        });
+                    });
+                } else {
+                    console.log('channel exists, rendering page');
                     exRes.render('figsignup', {figname: figName}, function(err, html) {
                         if (err) {
                             console.log(err);
@@ -59,23 +125,18 @@ app.get('/:figname', function(req, exRes) {
                 
                         exRes.send(html);
                     });
-                });
+                }
+    
             } else {
-                console.log('channel exists, rendering page');
-                exRes.render('figsignup', {figname: figName}, function(err, html) {
-                    if (err) {
-                        console.log(err);
-                    }
-            
-                    exRes.send(html);
-                });
+                console.log(err);
+                exRes.send('There was a problem');
             }
+        });
 
-        } else {
-            console.log(err);
-            exRes.send('There was a problem');
-        }
-    });
+    } else {
+        console.log('fig not found');
+        exRes.redirect('/notfound');
+    }
 
     
 });
